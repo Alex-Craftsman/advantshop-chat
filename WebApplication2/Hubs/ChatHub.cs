927e8d5dd3e642e489e812c577922f0019128f5a
webtemple <web.temple.com@gmail.com>
2018-06-04 16:24:40 +0400

    Init upload

---
+++ WebApplication2/Hubs/ChatHub.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using WebApplication2.Models;
+
+namespace WebApplication2.Hubs
+{
+    public class ChatHub : Hub
+    {
+        static List<User> Users = new List<User>();
+        static List<string> Messages = new List<string>();
+
+        // Отправка сообщений
+        public void Send(string name, string message)
+        {
+            DateTime now = DateTime.Now;
+            string timeNow = now.ToString("HH:mm:ss");
+            string _message = string.Format("{0} {1}: {2}", timeNow, name, message);
+            Messages.Add(_message);
+            Clients.All.addMessage(_message);
+        }
+
+        // Подключение нового пользователя
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+            if (!Users.Any(x => x.ConnectionId == id))
+            {
+                if(!Users.Any(x => x.Name == userName))
+                {
+                    Users.Add(new User { ConnectionId = id, Name = userName });
+
+                    Clients.Caller.onConnected(id);
+
+
+                    Clients.AllExcept(id).onNewUserConnected(id, userName);
+                }
+                else
+                {
+                    string _message = "Имя занято";
+                    Clients.Caller.onDoubleName(_message);
+                }
+            }
+        }
+
+        public void Onload()
+        {
+            foreach (string _message in Messages)
+            {
+                Clients.Caller.GetMessages(_message);
+            }
+        }
+
+        public void Initusers()
+        {
+            foreach(var user in Users)
+            {
+                string _name = user.Name;
+                Clients.Caller.GetUsersName(_name);
+            }
+        }
+
+        // Отключение пользователя
+        public override System.Threading.Tasks.Task OnDisconnected(bool stopCalled)
+        {
+            var item = Users.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+            if (item != null)
+            {
+                Users.Remove(item);
+                var id = Context.ConnectionId;
+                Clients.All.onUserDisconnected(id, item.Name);
+            }
+
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
---
+++ WebApplication2/app/app.js
@@ -0,0 +1,94 @@
+﻿function elemscrolltop() {
+    var height = document.getElementById('chat').scrollHeight - 50;
+    document.getElementById('chat').scrollTop = height;
+}
+
+
+
+var app = angular.module('app', []);
+app.controller('homecontroller', function ($scope, $http) {
+    $scope.message = { text: '' };
+    $scope.messages = [];
+    $scope.user = { name: '' };
+    $scope.users = [];
+    $scope.userId = {};
+    $scope.reg = true;
+    $scope.chat = $.connection.chatHub;
+
+    function InitMessages() {
+        $scope.chat.server.onload();
+    }
+
+    function addUser() {
+
+        function initUsers() {
+            $scope.chat.server.initusers();
+        }
+        console.log("s");
+        if (localStorage.getItem("user") !== null) {
+            $scope.user = JSON.parse(localStorage.getItem("user"));
+            $scope.chat.server.connect($scope.user.name);
+            $scope.reg = false;
+        }
+
+        else {
+            localStorage.setItem("user", JSON.stringify($scope.user));
+            $scope.user = JSON.parse(localStorage.getItem("user"));
+            $scope.chat.server.connect($scope.user.name);
+            $scope.reg = false;
+        }
+    }
+    
+    $scope.chat.client.onNewUserConnected = function (id, name) {
+        $scope.messages.push(name + " присоединился!");
+        elemscrolltop();
+    }
+
+    $scope.chat.client.GetMessages = function (message) {
+        $scope.messages.push(message);
+        elemscrolltop();
+        $scope.$apply();
+    }
+
+    $scope.chat.client.addMessage = function (message) {
+        $scope.messages.push(message);
+        elemscrolltop();
+        $scope.$apply();
+    }
+
+    $scope.chat.client.GetUsersName = function (user) {
+        $scope.users.push(user);
+        $scope.$apply();
+    }
+    
+    $scope.chat.client.onConnected = function (id) {
+        $scope.userId = id;
+        $scope.$apply();
+    }
+
+    $scope.chat.client.onDoubleName = function (error) {
+        alert(error);
+        localStorage.removeItem("user");
+        location.reload();
+    }
+
+    $.connection.hub.start().done(function () {
+        if (localStorage.getItem("user") !== null) {
+            $scope.user = JSON.parse(localStorage.getItem("user"));
+            addUser();
+            InitMessages();
+        }
+        $scope.adduser = function () {
+            addUser();
+            InitMessages();
+        }
+    });
+
+    $scope.sendmessage = function () {
+        $scope.chat.server.send($scope.user.name, $scope.message.text);
+        $scope.message = {}
+    };
+});
+
+
+
